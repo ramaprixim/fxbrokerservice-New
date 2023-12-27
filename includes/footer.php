8415916c2182882081fbb40ec27d9bc78565cb18
ramaprixim <rama.prixim@gmail.com>
2023-12-27 20:05:11 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,143 @@
+<footer class="footer-1">
+            <div class="footer-area">
+                <div class="container">
+                    <div class="row">
+                       <!-- Start column-->
+                       <div class="col-md-4 col-sm-6 col-xs-12">
+                            <div class="footer-content logo-footer">
+                                <div class="footer-head">
+                                    <div class="footer-logo">
+                                    	<a class="footer-black-logo" href="#"><img src="img/logo/logo2.png" alt=""></a>
+                                    </div>
+                                    <p>
+                                        Replacing a  maintains the amount of lines. When replacing a selection. help agencies to define their new business objectives and then create. Replacing a  maintains the amount of lines. 
+                                    </p>
+                                    <div class="subs-feilds">
+                                        <div class="suscribe-input">
+                                            <input type="email" class="email form-control width-80" id="sus_email" placeholder="Type Email">
+                                            <button type="submit" id="sus_submit" class="add-btn">Subscribe</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End column-->
+                        <!-- Start column-->
+                        <div class="col-md-2 col-sm-3 col-xs-12">
+                            <div class="footer-content">
+                                <div class="footer-head">
+                                    <h4>Products</h4>
+                                    <ul class="footer-list">
+                                        <li><a href="#">Bank Card</a></li>
+                                        <li><a href="#">Deposit Skim</a></li>
+                                        <li><a href="#">Affiliate</a></li>
+                                        <li><a href="#">Software</a></li>
+                                        <li><a href="#">Branding </a></li>
+                                        <li><a href="#">Promotion </a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End column-->
+                        <!-- Start column-->
+                        <div class="col-md-2 col-sm-3 col-xs-12">
+                            <div class="footer-content">
+                                <div class="footer-head">
+                                    <h4>Payments</h4>
+                                    <ul class="footer-list">
+                                        <li><a href="#">Send Mony</a></li>
+                                        <li><a href="#">Receive Money </a></li>
+                                        <li><a href="#">Shopping</a></li>
+                                        <li><a href="#">Online payment</a></li>
+                                        <li><a href="#">pay a Friend </a></li>
+                                        <li><a href="#">pay a bill </a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End column-->
+                        <!-- Start column-->
+                        <div class="col-md-2 hidden-sm col-xs-12">
+                            <div class="footer-content">
+                                <div class="footer-head">
+                                    <h4>Company</h4>
+                                    <ul class="footer-list">
+                                        <li><a href="#">About us</a></li>
+                                        <li><a href="#">Services </a></li>
+                                        <li><a href="#">Events</a></li>
+                                        <li><a href="#">Promotion</a></li>
+                                        <li><a href="#">Transition</a></li>
+                                        <li><a href="#">Social Media</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End column-->
+                        <!-- Start column-->
+                        <div class="col-md-2 hidden-sm col-xs-12">
+                             <div class="footer-content last-content">
+                                <div class="footer-head">
+                                    <h4>Support</h4>
+                                    <ul class="footer-list">
+                                        <li><a href="#">Customer Care</a></li>
+                                        <li><a href="#">Live chat</a></li>
+                                        <li><a href="#">Notification</a></li>
+                                        <li><a href="#">Privacy</a></li>
+                                        <li><a href="#">Terms & Condition</a></li>
+                                        <li><a href="#">Contact us </a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End column-->
+                    </div>
+                </div>
+            </div>
+            <!-- Start footer bottom area -->
+            <div class="footer-area-bottom">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                            <div class="copyright">
+                                <p>
+                                    Copyright © 2020
+                                    <a href="#">Bultifore</a> All Rights Reserved
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End footer bottom area -->
+        </footer>
+        <!-- End footer area -->
+        
+		
+		<!-- all js here -->
+
+		<!-- jquery latest version -->
+		<script src="js/vendor/jquery-1.12.4.min.js"></script>
+		<!-- bootstrap js -->
+		<script src="js/bootstrap.min.js"></script>
+		<!-- owl.carousel js -->
+		<script src="js/owl.carousel.min.js"></script>
+        <!-- stellar js -->
+        <script src="js/jquery.stellar.min.js"></script>
+		<!-- magnific js -->
+        <script src="js/magnific.min.js"></script>
+        <!-- Nice-select js -->
+        <script src="js/jquery.nice-select.min.js"></script>
+        <!-- wow js -->
+        <script src="js/wow.min.js"></script>
+        <!-- meanmenu js -->
+        <script src="js/jquery.meanmenu.js"></script>
+		<!-- Form validator js -->
+		<script src="js/form-validator.min.js"></script>
+		<!-- plugins js -->
+		<script src="js/plugins.js"></script>
+		<!-- main js -->
+		<script src="js/main.js"></script>
+	</body>
+
+<!-- Mirrored from rockstheme.com/rocks/bultifore-preview/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 27 Dec 2023 11:11:36 GMT -->
+</html>
