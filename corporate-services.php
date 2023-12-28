6439e01a7eeccd6441b438d4b679af52de716734
Ziasolkar <ziaprixim@gmail.com>
2023-12-28 16:45:52 +0530

    content

--- corporate-services.php
+++ corporate-services.php
@@ -19,4 +19,85 @@
     </div>
 </div>
 
+<div class="about-area bg-color2 area-padding">
+    <div class="container">
+        <div class="row">
+            <!-- Start column-->
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <div class="about-image">
+                    <img src="img/about/vd.jpg" alt="" class="ab-first-img">
+                    <img src="img/about/vd1.jpg" alt="" class="ab-second-img">
+                </div>
+            </div>
+            <!-- End column-->
+            <!-- Start column-->
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <div class="about-content">
+                    <h2>
+                        <span class="color-text">One-Stop Solution for Corporate Services</span>
+                    </h2>
+                    <p class="hidden-sm">No matter how simple or complex your needs, our expert team of industry experts
+                        have the talent to turn your business experience into a successful operational business.</p>
+
+                    <p class="hidden-sm">FXBrokerService has connections with experienced company formation agents
+                        worldwide. FXBrokerService has successfully assisted many corporate entities in various
+                        jurisdictions across the forex market.</p>
+
+                    <p class="hidden-sm">After analyzing with you, we suggest the jurisdiction that best suits your
+                        business goals. Once you have decided which jurisdiction to choose, continue with the process.
+                    </p>
+
+                    <p class="hidden-sm">With specialist incorporation agents in every jurisdiction, our highly skilled
+                        team has helped clients around the world set up and manage their forex businesses. We also
+                        support banking requirements (for foreign exchange businesses) and offer fully nominated and
+                        virtual office services to help you get your online business up and running professionally and
+                        efficiently.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="about-area bg-color2 area-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <div class="about-content">
+                    <h2>
+                        <span class="color-text">Open An Offshore (overseas) Bank Account</span>
+                    </h2>
+                    <p class="hidden-sm">In the growing forex industry, there are many trades made every day that
+                        require an offshore business bank account with a financially stable institution.</p>
+
+                    <p class="hidden-sm">Make your brokerage successful with our services. We can help you get a bank
+                        account quickly, depending on your jurisdiction's needs.</p>
+
+                    <p class="hidden-sm">Contact the FXBrokerService team today to get your offshore trading account.
+                    </p>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <div class="about-image">
+                    <img src="img/about/vd.jpg" alt="" class="ab-first-img">
+                    <img src="img/about/vd1.jpg" alt="" class="ab-second-img">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="brand-area area-padding fix" data-stellar-background-ratio="0.6">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="section-headline text-center">
+                    <h3>We’re here to help and answer any question you might have.</h3>
+                    <a class="hire-btn" href="#">Let's Talk Now</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 <?php include("includes/footer.php"); ?>
